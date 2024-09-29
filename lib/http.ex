e801a3f4027d63d840eab71c57a475cc92674fe2
jenny <jenny.hu@scrumcn.com>
2024-09-29 16:33:51 +0800

    添加3个参数的调用方法

--- lib/http.ex
+++ lib/http.ex
@@ -1,4 +1,9 @@
 defmodule ExAlipay.HTTP.Behaviour do
   @typep url :: binary()
+  @typep opt1 :: list()
+  @typep opt2 :: map()
+
   @callback get(url) :: {:ok, map()} | {:error, binary() | map()}
+
+  @callback get(url, opt1, opt2) :: {:ok, map()} | {:error, binary() | map()}
 end
